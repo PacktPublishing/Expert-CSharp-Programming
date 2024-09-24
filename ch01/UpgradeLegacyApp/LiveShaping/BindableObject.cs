d6dfc670f50e1bea040a46def04a98af41ef2b80
Christian Nagel <christian@christiannagel.com>
2024-09-24 22:30:08 +0200

    update c# syntax for wpf app

--- ch01/UpgradeLegacyApp/LiveShaping/BindableObject.cs
+++ ch01/UpgradeLegacyApp/LiveShaping/BindableObject.cs
@@ -1,24 +1,22 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace LiveShaping
-{
-    public abstract class BindableObject : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+namespace LiveShaping;
 
-        protected virtual void SetProperty<T>(ref T item, T value, [CallerMemberName] string propertyName = null)
+public abstract class BindableObject : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged(string? propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual void SetProperty<T>(ref T item, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (!EqualityComparer<T>.ReferenceEquals(item, value))
         {
-            if (!EqualityComparer<T>.ReferenceEquals(item, value))
-            {
-                item = value;
-                OnPropertyChanged(propertyName);
-            }
+            item = value;
+            OnPropertyChanged(propertyName);
         }
     }
 }
--- ch01/UpgradeLegacyApp/LiveShaping/LapRacerInfo.cs
+++ ch01/UpgradeLegacyApp/LiveShaping/LapRacerInfo.cs
@@ -1,33 +1,32 @@
-﻿namespace LiveShaping
-{
-    public enum PositionChange
-    {
-        None,
-        Up,
-        Down,
-        Out
-    }
+﻿namespace LiveShaping;
 
-    public class LapRacerInfo : BindableObject
+public enum PositionChange
+{
+    None,
+    Up,
+    Down,
+    Out
+}
+
+public class LapRacerInfo : BindableObject
+{
+    public required Racer Racer { get; init; }
+    private int _lap;
+    public int Lap
     {
-        public Racer Racer { get; set; }
-        private int _lap;
-        public int Lap
-        {
-            get { return _lap; }
-            set { SetProperty(ref _lap, value); }
-        }
-        private int _position;
-        public int Position
-        {
-            get { return _position; }
-            set { SetProperty(ref _position, value); }
-        }
-        private PositionChange _positionChange;
-        public PositionChange PositionChange
-        {
-            get { return _positionChange; }
-            set { SetProperty(ref _positionChange, value); }
-        }
+        get => _lap;
+        set => SetProperty(ref _lap, value);
+    }
+    private int _position;
+    public int Position
+    {
+        get => _position;
+        set => SetProperty(ref _position, value);
+    }
+    private PositionChange _positionChange;
+    public PositionChange PositionChange
+    {
+        get => _positionChange;
+        set => SetProperty(ref _positionChange, value);
     }
 }
