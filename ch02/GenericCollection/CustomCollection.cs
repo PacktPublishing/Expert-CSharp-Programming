043184008d8a4983b3498327a62f0aeda2cc9203
Christian Nagel <christian@christiannagel.com>
2024-10-11 21:39:26 +0200

    generic collection

---
+++ ch02/GenericCollection/CustomCollection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace GenericCollection;
+internal class CustomCollection<T> : IReadOnlyList<T>
+{
+    private readonly T[] _items;
+
+    public CustomCollection(int capacity)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(capacity);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(capacity, Array.MaxLength);
+        if (capacity == 0)
+        {
+            _items = [];
+            return;
+        }
+        _items = new T[capacity];
+    }
+
+    public T this[int index]
+    {
+        get => _items[index];
+        set => _items[index] = value;
+    }
+
+    public int Count =>
+        _items.Count(x => x != null);
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < _items.Length; i++)
+        {
+            if (_items[i] is not null)
+            {
+                yield return _items[i];
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() =>
+        _items.GetEnumerator();
+}
---
+++ ch02/GenericCollection/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericCollection;
+
+int size = 10;
+CustomCollection<int> data = new(size);
+
+for (int i = 0; i < size; i++)
+{
+    data[i] = i;
+}
+
+foreach (var item in data)
+{
+    Console.WriteLine(item);
+}
+
