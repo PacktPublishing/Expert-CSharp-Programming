7e54f0c9de7f11f2242dd0818991d07e5cde6640
Christian Nagel <christian@christiannagel.com>
2024-10-12 14:43:49 +0200

    behind the scenes

---
+++ ch02/BehindTheScenes/Program.cs
@@ -0,0 +1,17 @@
+﻿int x = 42;
+PassAnObject(x);
+PassAnyType(x);
+
+string s = "Hello";
+PassAnObject(s);
+PassAnyType(s);
+
+static void PassAnObject(object o)
+{
+    Console.WriteLine(o);
+}
+
+static void PassAnyType<T>(T t)
+{
+    Console.WriteLine(t);
+}
