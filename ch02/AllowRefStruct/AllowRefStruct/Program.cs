ae3e01ffdd470ca73bdf809e73c3b009f4451cae
Christian Nagel <christian@christiannagel.com>
2024-10-16 11:28:22 +0200

    allow ref struct sample

---
+++ ch02/AllowRefStruct/AllowRefStruct/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] data = [1, 2, 3, 4, 5, 6, 7];
+
+Func<Span<int>, Span<int>> incrementFirstThree = (Span<int> s) =>
+{
+    var slice = s[0..3];
+    for (int i = 0; i < slice.Length; i++)
+    {
+        slice[i]++;
+    }
+    return s;
+};
+
+incrementFirstThree(data.AsSpan());
+
+foreach (var item in data)
+{
+    Console.WriteLine(item);
+}   
