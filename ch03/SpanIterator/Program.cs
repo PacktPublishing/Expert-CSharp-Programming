b6d769955c252d14399dd7d05954dcae3ac4faab
Christian Nagel <christian@christiannagel.com>
2024-11-01 23:23:30 +0100

    span iterator

---
+++ ch03/SpanIterator/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] data = [1, 5, 8, 11, 22, 23, 27, 33];
+
+Span<int> span = data.AsSpan();
+
+SpanIterator<int> iterator = new SpanIterator<int>(span);
+while (iterator.MoveNext(out int value))
+{
+    Console.WriteLine(value);
+}
---
+++ ch03/SpanIterator/SpanIterator.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+public ref struct SpanIterator<T>(Span<T> span)
+{
+    private readonly Span<T> _span = span;
+    private int _index = 0;
+
+    public bool MoveNext([MaybeNullWhen(false)] out T value)
+    {
+        if (_index < _span.Length)
+        {
+            value = _span[_index++];
+            return true;
+        }
+        value = default;
+        return false;
+    }
+}
