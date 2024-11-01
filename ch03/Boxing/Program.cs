0b1e506e25280ad931559d89c7c5e73a7384df9d
Christian Nagel <christian@christiannagel.com>
2024-11-01 22:06:28 +0100

    boxing sample

---
+++ ch03/Boxing/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<BenchmarkList>();
+
+[MemoryDiagnoser]
+public class BenchmarkList
+{
+    [Benchmark]
+    public void Boxing()
+    {
+        BoxingList();
+    }
+
+    [Benchmark]
+    public void Values()
+    {
+        ValuesList();
+    }
+
+    private static int BoxingList()
+    {
+        List<object> list = new(1000);
+        for (int i = 0; i < 1000; i++)
+        {
+            list.Add(i);
+        }
+
+        int sum = 0;
+        foreach (int item in list)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+
+    private static int ValuesList()
+    {
+        List<int> list = new(1000);
+        for (int i = 0; i < 1000; i++)
+        {
+            list.Add(i);
+        }
+
+        int sum = 0;
+        foreach (int item in list)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+}
+
+
+
