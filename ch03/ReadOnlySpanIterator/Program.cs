60e9c8dcde38afab6bb7b034feff681855d3dc8d
Christian Nagel <christian@christiannagel.com>
2024-11-02 22:58:56 +0100

    read only span iterator

---
+++ ch03/ReadOnlySpanIterator/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] data = [1, 5, 8, 11, 22, 23, 27, 33];
+
+ReadOnlySpan<int> span = data.AsSpan();
+
+ReadOnlySpanIterator<int> iterator = new(span);
+while (iterator.MoveNext(out int value))
+{
+    Console.Write($"{value} ");
+}
+
+iterator = new(span);
+while (iterator.MoveNext(out _))
+{
+    ref readonly int item = ref iterator.Current;
+    Console.Write($"{item} ");
+}
