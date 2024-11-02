11ed4f30d50bc5303e4affc14e427df8bb5f05c1
Christian Nagel <christian@christiannagel.com>
2024-11-02 18:52:40 +0100

    ref keyword

--- ch03/SpanIterator/Program.cs
+++ ch03/SpanIterator/Program.cs
@@ -2,8 +2,26 @@
 
 Span<int> span = data.AsSpan();
 
-SpanIterator<int> iterator = new SpanIterator<int>(span);
+SpanIterator<int> iterator = new(span);
 while (iterator.MoveNext(out int value))
 {
-    Console.WriteLine(value);
+    Console.Write($"{value} ");
 }
+Console.WriteLine();
+
+iterator = new(span);
+while (iterator.MoveNext(out _))
+{
+    ref int item = ref iterator.Current;
+    if (item % 2 == 0)
+    {
+        item *= 2;
+    }
+}
+
+Console.WriteLine("Modified the array");
+foreach (int item in data)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
--- ch03/SpanIterator/SpanIterator.cs
+++ ch03/SpanIterator/SpanIterator.cs
@@ -3,16 +3,29 @@
 public ref struct SpanIterator<T>(Span<T> span)
 {
     private readonly Span<T> _span = span;
-    private int _index = 0;
+    private int _index = -1;
 
     public bool MoveNext([MaybeNullWhen(false)] out T value)
     {
-        if (_index < _span.Length)
+        _index++;
+        if (_index >= 0 && _index < _span.Length)
         {
-            value = _span[_index++];
+            value = _span[_index];
             return true;
         }
         value = default;
         return false;
+    }
+
+    public ref T Current
+    {
+        get
+        {
+            if (_index < 0 || _index >= _span.Length)
+            {
+                throw new InvalidOperationException();
+            }
+            return ref _span[_index];
+        }
     }
 }
