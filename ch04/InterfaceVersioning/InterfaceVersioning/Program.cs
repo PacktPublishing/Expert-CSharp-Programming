8debd424e3e0a0fe22c60e2b3851efc539bf2ee6
Christian Nagel <christian@christiannagel.com>
2025-01-04 20:10:53 +0100

    interface versioning

---
+++ ch04/InterfaceVersioning/InterfaceVersioning/Program.cs
@@ -0,0 +1,13 @@
+﻿using InterfaceVersioning;
+
+Customer customer1 = new("John Doe", DateTime.Now.AddYears(-2));
+
+RegularCustomer customer2 = new("Jane Doe", DateTime.Now.AddYears(-2));
+
+CustomerWithDiscount(customer1);
+CustomerWithDiscount(customer2);
+
+void CustomerWithDiscount(ICustomer customer)
+{
+    Console.WriteLine($"Customer: {customer.Name}, Discount: {customer.Discount()}");
+}
