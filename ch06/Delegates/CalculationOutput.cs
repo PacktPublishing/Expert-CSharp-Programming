a8a311e80b09755a898309724fb9bd051e9fcc43
Christian Nagel <christian@christiannagel.com>
2025-02-20 15:48:41 +0100

    ch06 initial delegates

---
+++ ch06/Delegates/CalculationOutput.cs
@@ -0,0 +1,14 @@
+﻿namespace Delegates;
+
+internal class CalculationOutput
+{
+    public static void Addition(int x, int y)
+    {
+        Console.WriteLine($"{x} + {y} results in {x + y}");
+    }
+
+    public static void Subtraction(int x, int y)
+    {
+        Console.WriteLine($"{x} - {y} results in {x - y}");
+    }
+}
---
+++ ch06/Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using Delegates;
+
+Func<int, int, int> add = Calculator.Add;
+Console.WriteLine(add(10, 5));
+
+Func<int, int, int> subtract = (x, y) => x - y;
+Console.WriteLine(subtract(10, 5));
+
+MathOp multiply = (x, y) => x * y;
+Console.WriteLine(multiply(10, 5));
+
+Action<int, int> calcOutput = CalculationOutput.Addition;
+calcOutput += CalculationOutput.Subtraction;
+calcOutput.Invoke(7, 2);
+
+Action<int, int> combinedDelegate = (Action<int, int>)Delegate.Combine(CalculationOutput.Add, CalculationOutput.Subtraction);
+combinedDelegate.Invoke(11, 22);
+
+var delegates = combinedDelegate.GetInvocationList();
+
