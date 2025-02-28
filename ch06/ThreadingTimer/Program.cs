cb49f6c7f60f560f04e3c68604022d1cf41648c7
Christian Nagel <christian@christiannagel.com>
2025-02-28 10:28:40 +0100

    ch06 threading timer

---
+++ ch06/ThreadingTimer/Program.cs
@@ -0,0 +1,7 @@
+﻿Timer timer = new(static (object? _) =>
+{
+    Console.WriteLine($"timer called at {DateTime.Now:HH:mm:ss}");
+}, state: null, dueTime: 3000, period: 1500);
+
+await Task.Delay(10000);
+timer.Dispose();
