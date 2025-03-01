e7608badae69882d771ecbf6c8b94a8aa1fc2533
Christian Nagel <christian@christiannagel.com>
2025-03-01 12:45:51 +0100

    ch06 events sample

---
+++ ch06/EventsSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using EventsSample;
+
+Observer observer = new();
+
+var subjects = Enumerable.Range(1, 100)
+    .Select(x =>
+    {
+        Subject s = new(x);
+        s.MyEvent += observer.Handler;
+        return s;
+    })
+    .ToArray();
+
+foreach (var subject in subjects)
+{
+    subject.RaiseEvent();
+}
+
+for (int i = 0; i < subjects.Length; i++)
+{
+    subjects[i].MyEvent -= observer.Handler;
+}
