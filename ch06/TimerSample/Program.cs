daf19fcece66f5a1e41729dd64652708277352e4
Christian Nagel <christian@christiannagel.com>
2025-03-01 13:02:55 +0100

    ch06 - System.Timers.Timer

---
+++ ch06/TimerSample/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//using Timer timer = new(OnTimer, null, dueTime: 2000, period: 500);
+//Console.ReadLine();
+
+//void OnTimer(object? state)
+//{
+//    Console.WriteLine("TimerCallback: " + DateTime.Now);
+//}
+
+using Timer = System.Timers;
+
+PeriodicTimer timer = new(OnTimer, null, dueTime: 2000, period: 500);
+timer.
+System.Timers.Timer timer = new(2000);
