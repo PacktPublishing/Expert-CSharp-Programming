102808546ee6021da567238f52f9baa14ece3028
Christian Nagel <christian@christiannagel.com>
2025-03-01 21:43:12 +0100

    ch06 WeakEventsManager

---
+++ ch06/WeakEventsManager/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Media;
+
+namespace WeakEvents;
+
+public class Subject(int id)
+{
+    public int Id { get; } = id;
+    private string _text = $"Hello, World! {id}";
+    //private int[] _data = [.. Enumerable.Range(0, 100).Select(x => x)];
+
+    public event EventHandler<SubjectEventArgs>? SomeEvent;
+
+    public void RaiseEvent()
+    {
+        SomeEvent?.Invoke(this, new SubjectEventArgs(Id));
+    }
+}
+
+public class SubjectEventArgs(int id) : EventArgs
+{
+    public int Id { get; } = id;
+}
