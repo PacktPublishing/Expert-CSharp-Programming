eb4923f8f1db514df1ae2dd29a98e9c4550f12c5
Christian Nagel <christian@christiannagel.com>
2025-03-02 11:58:36 +0100

    ch06 WPF weak events

---
+++ ch06/WPFWeakEvents/Subject.cs
@@ -0,0 +1,13 @@
+﻿namespace WPFWeakEvents;
+
+public class Subject(int id)
+{
+    public int Id { get; } = id;
+
+    public event EventHandler<SubjectEventArgs>? SomeEvent;
+
+    public void RaiseEvent()
+    {
+        SomeEvent?.Invoke(this, new SubjectEventArgs(Id));
+    }
+}
---
+++ ch06/WPFWeakEvents/SubjectEventArgs.cs
@@ -0,0 +1,6 @@
+﻿namespace WPFWeakEvents;
+
+public class SubjectEventArgs(int id) : EventArgs
+{
+    public int Id { get; } = id;
+}
