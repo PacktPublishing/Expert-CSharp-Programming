79a446ae7c75526832416ae933ff3d2a504d20b6
Christian Nagel <christian@christiannagel.com>
2025-03-16 07:22:13 +0100

    collection expressions

---
+++ ch07/CollectionExpressions/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = [1, 2, 3, 4, 5];
+List<string> words = ["one", "two", "three", "four"];
+IEnumerable<Person> people =[new Person("John", "Doe"), new Person("Jane", "Doe")];
+
+List<int> numbers2 = [1, 2, 3, 4, 5];
+List<int> combined = [.. numbers, .. numbers2];
+
+public record class Person(string FirstName, string LastName);
