625ca374d6fecdd6cbfe820c8af0f79b7b5041c9
Christian Nagel <christian@christiannagel.com>
2025-04-19 13:53:59 +0200

    ch07 collection expressions

--- ch07/CollectionExpressions/Program.cs
+++ ch07/CollectionExpressions/Program.cs
@@ -1,8 +1,26 @@
-﻿int[] numbers = [1, 2, 3, 4, 5];
+﻿int[] numbers1 = [1, 2, 3, 4, 5];
 List<string> words = ["one", "two", "three", "four"];
 IEnumerable<Person> people =[new Person("John", "Doe"), new Person("Jane", "Doe")];
 
-List<int> numbers2 = [1, 2, 3, 4, 5];
-List<int> combined = [.. numbers, .. numbers2];
+List<int> numbers2 = [6, 7, 8, 9];
+List<int> combined = [.. numbers1, .. numbers2];
+ShowItems(nameof(combined), combined); // 1, 2, 3, 4, 5, 6, 7, 8, 9
 
-public record class Person(string FirstName, string LastName);
+List<int> numbers3 = [.. numbers1[2..^1]]; // 3, 4
+int x = numbers1[^1]; // select the last element
+
+ShowItems(nameof(numbers3), numbers3);
+
+CustomCollection<string> customCollection = ["one", "two", "three"];
+
+ShowItems(nameof(CustomCollection), customCollection);
+
+void ShowItems<T>(string title, IEnumerable<T> items)
+{
+    Console.WriteLine(title);
+    foreach (T item in items)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine();
+}
