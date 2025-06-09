173481120c7bdeef74f64bb174cda385b8787f0e
Christian Nagel <christian@christiannagel.com>
2025-06-09 23:25:03 +0200

    ch08 deferred LINQ

---
+++ ch08/DeferredLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using CustomImplementation;
+
+int[] data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var filteredData = CustomEnumerator.Filter1<int>(null!, null!);
+
+try
+{
+    foreach (var item in filteredData)
+    {
+        Console.Write($"{item} ");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+try
+{
+    var filteredData2 = CustomEnumerator.Filter2<int>(null!, null!);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+
