bf819fad1469a69b2ba8e12d4f10ff7334c6bb86
Christian Nagel <christian@christiannagel.com>
2025-06-09 22:33:13 +0200

    ch08 CustomImplementation

--- ch08/CustomImplementation/Program.cs
+++ ch08/CustomImplementation/Program.cs
@@ -1,31 +1,36 @@
-﻿using System.Collections;
+﻿using CustomImplementation;
 
-IEnumerable<int> data = [1, 2, 3, 4, 5];
+IEnumerable<int> data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+Console.WriteLine("Iterating through all elements");
 foreach (var item in data)
 {
-    Console.WriteLine(item);
+    Console.Write($"{item} ");
 }
+Console.WriteLine();
 
+Console.WriteLine("Using while method");
 using IEnumerator<int> enumerator = data.GetEnumerator();
 while (enumerator.MoveNext())
 {
-    Console.WriteLine(enumerator.Current);
+    Console.Write($"{enumerator.Current} ");
 }
+Console.WriteLine();
 
-IEnumerable<int>? data1 = null;
-var enumerator1 = CustomImplementation.CustomEnumerator.Where<int>(null!, null!);
 
-Console.WriteLine("this statement runs!");
-
-foreach (int x in enumerator1)
+Console.WriteLine("Using Filter method with yield");
+foreach (int item in Filter(data, x => x % 2 == 0))
 {
-    Console.WriteLine(x);
+    Console.Write($"{item} ");
 }
+Console.WriteLine();
 
-// TODO: show deferred invocation
-
-
+Console.WriteLine("Using FilterEnumerable class ");
+foreach (var item in new FilterEnumerable<int>(data, x => x % 2 == 0))
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
 
 static IEnumerable<T> Filter<T>(IEnumerable<T> source, Func<T, bool> predicate)
 {
@@ -37,171 +42,3 @@ static IEnumerable<T> Filter<T>(IEnumerable<T> source, Func<T, bool> predicate)
         }
     }
 }
-
-public class Enumerable<TSource, TResult>(
-    IEnumerable<TSource> source,
-    Func<TSource, TResult> predicate)
-    : IEnumerable<TResult>
-{
-    public IEnumerator<TResult> GetEnumerator() =>
-        new FilterEnumerator<TSource, TResult>(source, predicate);
-
-    IEnumerator IEnumerable.GetEnumerator() =>
-        GetEnumerator();
-}
-
-public sealed class FilterEnumerator<TSource, TResult>(
-    IEnumerable<TSource> source,
-    Func<TSource, TResult> predicate) :
-    IEnumerator<TResult>
-{
-    private IEnumerator<TSource>? _enumerator;
-    private int _state = 1;
-
-    public TResult Current
-    { get; private set; } = default!;
-    object? IEnumerator.Current => Current;
-
-    public void Dispose()
-    {
-        _enumerator?.Dispose();
-    }
-
-    public bool MoveNext()
-    {
-        switch (_state)
-        {
-            case 1:
-                _enumerator = source.GetEnumerator();
-                _state = 2;
-                goto case 2; // Fall through to the next case
-            case 2:
-                try
-                {
-                    if (_enumerator?.MoveNext() ?? false)
-                    {
-                        Current = predicate(_enumerator.Current);
-                        return true;
-                    }
-                }
-                catch
-                {
-                    Dispose();
-                    throw;
-                }
-                break;
-        }
-
-        Dispose();
-        return false;
-    }
-
-    //public bool MoveNext()
-    //{
-    //    _enumerator = source.GetEnumerator();
-    //    try
-    //    {
-    //        if (_enumerator.MoveNext())
-    //        {
-    //            Current = predicate(_enumerator.Current);
-    //            return true;
-    //        }
-    //    }
-    //    catch
-    //    {
-    //        Dispose();
-    //        throw;
-    //    }
-    //    return false;
-    //}
-
-    public void Reset()
-    {
-        // Legacy from COM
-        throw new NotSupportedException(); // Most implementations do not support Reset
-    }
-}
-
-//public class Enumerable<TSource, TResult>(
-//    IEnumerable<TSource> source, 
-//    Func<TSource, TResult> predicate) 
-//    : IEnumerable<TResult>
-//{
-//    public IEnumerator<TResult> GetEnumerator() => 
-//        new FilterEnumerator<TSource, TResult>(source, predicate);
-
-//    IEnumerator IEnumerable.GetEnumerator() => 
-//        GetEnumerator();
-//}
-
-//public sealed class FilterEnumerator<TSource, TResult>(
-//    IEnumerable<TSource> source, 
-//    Func<TSource, TResult> predicate) : 
-//    IEnumerator<TResult>
-//{
-//    private IEnumerator<TSource>? _enumerator;
-//    private int _state = 1;
-
-//    public TResult Current
-//    { get; private set; } = default!;
-//    object? IEnumerator.Current => Current;
-
-//    public void Dispose()
-//    {
-//        _enumerator?.Dispose();
-//    }
-
-//    public bool MoveNext()
-//    {
-//        switch (_state)
-//        {
-//            case 1:
-//                _enumerator = source.GetEnumerator();
-//                _state = 2;
-//                goto case 2; // Fall through to the next case
-//            case 2:
-//                try
-//                {
-//                    if (_enumerator?.MoveNext() ?? false)
-//                    {
-//                        Current = predicate(_enumerator.Current);
-//                        return true;
-//                    }
-//                }
-//                catch
-//                {
-//                    Dispose();
-//                    throw;
-//                }
-//                break;
-//        }
-
-//        Dispose();
-//        return false;
-//    }
-
-//    //public bool MoveNext()
-//    //{
-//    //    _enumerator = source.GetEnumerator();
-//    //    try
-//    //    {
-//    //        if (_enumerator.MoveNext())
-//    //        {
-//    //            Current = predicate(_enumerator.Current);
-//    //            return true;
-//    //        }
-//    //    }
-//    //    catch
-//    //    {
-//    //        Dispose();
-//    //        throw;
-//    //    }
-//    //    return false;
-//    //}
-
-//    public void Reset()
-//    {
-//        // Legacy from COM
-//        throw new NotSupportedException(); // Most implementations do not support Reset
-//    }
-//}
