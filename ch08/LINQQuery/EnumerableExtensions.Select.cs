bea8a2bc917fa856a305e50b1b3e2cb8eb570e33
Christian Nagel <christian@christiannagel.com>
2025-06-13 22:22:36 +0200

    LINQ Query

---
+++ ch08/LINQQuery/EnumerableExtensions.Select.cs
@@ -0,0 +1,21 @@
+﻿public static partial class EnumerableExtensions
+ {
+     extension<T>(IEnumerable<T> source)
+     {
+        public IEnumerable<TResult> Select<TResult>(Func<T, TResult> selector)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(selector);
+
+            return Iterator();
+
+            IEnumerable<TResult> Iterator()
+            {
+                foreach (var item in source)
+                {
+                    yield return selector(item);
+                }
+            }
+        }
+    }
+}
--- ch08/LINQQuery/Program.cs
+++ ch08/LINQQuery/Program.cs
@@ -2,22 +2,21 @@
 
 var champions = Formula1.GetFormula1Champions();
 
-//LinqQuerySyntax(champions);
-//LinqMethodSyntax(champions);
-// UsingCustomMethod(champions);
-UsingCustomMethodPassingNull(champions);
-UsingCustomMethodPassingNullWithEarlyValidation(champions);
+LinqQuerySyntax(champions);
+LinqMethodSyntax(champions);
 
 static void LinqQuerySyntax(IEnumerable<Racer> champions)
 {
-    var q = from r in champions
-            where r.Championships.Count > 3
-            orderby r.Championships.Count descending, r.LastName ascending
-            select new { r.FirstName, r.LastName, Championships = r.Championships.Count };
+    var q = 
+        from r in champions
+            let numberChampionShips = r.Championships.Count()
+            where numberChampionShips > 3
+            orderby numberChampionShips descending, r.LastName ascending
+            select new { r.FirstName, r.LastName, Championships = numberChampionShips };
 
-    foreach (var item in q)
+    foreach (var racer in q)
     {
-        Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Championships} Championships");
+        Console.WriteLine($"{racer.FirstName} {racer.LastName} - {racer.Championships} Championships");
     }
     Console.WriteLine();
 }
@@ -25,110 +24,14 @@ static void LinqQuerySyntax(IEnumerable<Racer> champions)
 static void LinqMethodSyntax(IEnumerable<Racer> champions)
 {
     var q = champions
-        .Where(r => r.Championships.Count > 3)
-        .OrderByDescending(r => r.Championships.Count)
-        .ThenBy(r => r.LastName)
-        .Select(r => new { r.FirstName, r.LastName, Championships = r.Championships.Count });
+        .Select(r => new { Racer = r, NumberChampionships = r.Championships.Count() })
+        .Where(x => x.NumberChampionships > 3)
+        .OrderByDescending(x => x.NumberChampionships)
+        .ThenBy(x => x.Racer.LastName)
+        .Select(x => new { x.Racer.FirstName, x.Racer.LastName, Championships = x.NumberChampionships });
     foreach (var item in q)
     {
         Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Championships} Championships");
     }
     Console.WriteLine();
-}
-
-static void UsingCustomMethod(IEnumerable<Racer> champions)
-{
-    var q = champions.Filter(r => r.Championships.Count > 6);
-
-    foreach (var item in q)
-    {
-        Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Championships} Championships");
-    }
-    Console.WriteLine();
-}
-
-static IEnumerable<Racer> GetNull() => null!;
-
-static void UsingCustomMethodPassingNull(IEnumerable<Racer> champions)
-{
-    IEnumerable<Racer> racers = GetNull();
-    var result = racers.Filter(r => r.LastName.StartsWith("Sen")); // This will throw an exception
-
-    foreach (var item in result)
-    {
-        Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Championships} Championships");
-    }
-
-    Console.WriteLine();
-}
-
-
-static void UsingCustomMethodPassingNullWithEarlyValidation(IEnumerable<Racer> champions)
-{
-    IEnumerable<Racer> racers = GetNull();
-    var result = racers!.Filter1(r => r.LastName.StartsWith("Sen")); // This will throw an exception
-
-    foreach (var item in result)
-    {
-        Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Championships} Championships");
-    }
-
-    Console.WriteLine();
-}
-
-public static class EnumerableExtensions
-{
-    extension<T>(IEnumerable<T> source)
-    {
-        public IEnumerable<T> Filter(Func<T, bool> predicate)
-        {
-            ArgumentNullException.ThrowIfNull(source);
-            ArgumentNullException.ThrowIfNull(predicate);
-
-            foreach (var item in source)
-            {
-                if (predicate(item))
-                {
-                    yield return item;
-                }
-            }
-        }
-
-        public IEnumerable<T> Filter1(Func<T, bool> predicate)
-        {
-            static void ValidateArguments(IEnumerable<T> source, Func<T, bool> predicate)
-            {
-                ArgumentNullException.ThrowIfNull(source);
-                ArgumentNullException.ThrowIfNull(predicate);
-            }
-
-            ValidateArguments(source, predicate);
-
-            foreach (var item in source)
-            {
-                if (predicate(item))
-                {
-                    yield return item;
-                }
-            }
-        }
-    }
-}
-
-public static class OlderExtensions
-{
-    // This is an older version of the extension method, which is not recommended for use in modern C#.
-    // It uses a different syntax and does not support nullable reference types.
-    // It's included here for educational purposes only.
-    [Obsolete("This method uses older syntax. Use the newer version instead.")]
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> source, Func<T, bool> predicate)
-    {
-        foreach (var item in source)
-        {
-            if (predicate(item))
-            {
-                yield return item;
-            }
-        }
-    }
 }
