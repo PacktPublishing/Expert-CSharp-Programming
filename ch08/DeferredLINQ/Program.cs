7a52422f91cc0e36f2d9a55ffecf06322e934113
Christian Nagel <christian@christiannagel.com>
2025-06-13 17:18:54 +0200

    deferred LINQ

--- ch08/DeferredLINQ/Program.cs
+++ ch08/DeferredLINQ/Program.cs
@@ -13,7 +13,7 @@ try
 }
 catch (Exception ex)
 {
-    Console.WriteLine(ex);
+    Console.WriteLine(ex.Message);
 }
 
 try
@@ -22,7 +22,26 @@ try
 }
 catch (Exception ex)
 {
-    Console.WriteLine(ex);
+    Console.WriteLine(ex.Message);
 }
 
+List<string> names = ["John", "Niki", "Max", "Jochen", "Jack", "Lewis", "Juan"];
 
+var query = names.Where(
+    name => name.StartsWith('J'))
+    .OrderBy(n => n);
+
+ShowNames("1st iteration", query);
+
+names.AddRange("Jenson", "Emerson", "Jody");
+
+ShowNames("2nd iteration", query);
+static void ShowNames(string title, IEnumerable<string> names)
+{
+    Console.WriteLine(title);
+    foreach (var name in names)
+    {
+        Console.Write($"{name} ");
+    }
+    Console.WriteLine();
+}
