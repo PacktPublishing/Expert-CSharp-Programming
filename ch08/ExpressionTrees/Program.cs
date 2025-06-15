aa936b51288490243c4a16baa6b48fc2ecbc6bf5
Christian Nagel <christian@christiannagel.com>
2025-06-15 16:52:42 +0200

    Add Expression Trees project and visualization method
    Created `ExpressionTrees.csproj` targeting .NET 10.0 with an executable output type and a reference to `DataLib`. Updated `Program.cs` to include a new expression tree for filtering `Racer` objects and added a `DisplayTree` method for visualizing the structure of expression trees, enhancing debugging capabilities.

---
+++ ch08/ExpressionTrees/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq.Expressions;
+
+using DataLib;
+
+Expression<Func<Racer, bool>> expression = r => r.Country == "Brazil" && r.Wins > 6;
+
+DisplayTree(0, "Lambda", expression);
+
+static void DisplayTree(int indent, string message, Expression expression)
+{
+    string output = $"{new string('>', indent)} {message}! " + 
+        $"NodeType: {expression.NodeType}; Expr: {expression}";
+
+    indent++;
+
+    switch (expression.NodeType)
+    {
+        case ExpressionType.Lambda:
+            Console.WriteLine(output);
+            LambdaExpression lambdaExpr = (LambdaExpression)expression;
+            foreach (var parameter in lambdaExpr.Parameters)
+            {
+                DisplayTree(indent, "Parameter", parameter);
+            }
+            DisplayTree(indent, "Body", lambdaExpr.Body);
+            break;
+        case ExpressionType.Constant:
+            ConstantExpression constExpr = (ConstantExpression)expression;
+            Console.WriteLine($"{output} Const Value: {constExpr.Value}");
+            break;
+        case ExpressionType.Parameter:
+            ParameterExpression paramExpr = (ParameterExpression)expression;
+            Console.WriteLine($"{output} Param Type: {paramExpr.Type.Name}");
+            break;
+        case ExpressionType.Equal:
+        case ExpressionType.AndAlso:
+        case ExpressionType.GreaterThan:
+            if (expression is BinaryExpression binaryExpression)
+            {
+                Console.WriteLine($"{output} Method: {binaryExpression.Method?.Name}");
+                DisplayTree(indent, "Left", binaryExpression.Left);
+                DisplayTree(indent, "Right", binaryExpression.Right);
+            }
+            else
+            {
+                Console.WriteLine(output);
+            }
+
+            break;
+        case ExpressionType.MemberAccess:
+            MemberExpression memberExpr = (MemberExpression)expression;
+            Console.WriteLine($"{output} Member Name: {memberExpr.Member.Name}, Type: {memberExpr.Type.Name}");
+            if (memberExpr.Expression is not null)
+            {
+                DisplayTree(indent, "Member Expr", memberExpr.Expression);
+            }
+            break;
+        default:
+            Console.WriteLine();
+            Console.WriteLine($"{expression.NodeType} {expression.Type.Name}");
+            break;
+    }
+}
